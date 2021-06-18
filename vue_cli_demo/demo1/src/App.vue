5c3f7c96dbd902685f4732b7cc2e4c8cc75f365b
CKzcb <zhuchb_0509@163.com>
2021-06-19 00:24:56 +0800

    demo1

---
+++ vue_cli_demo/demo1/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>{{message}}</h1>
+  <button @click="show_message">点击试试</button>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: 'hello world',
+    };
+  },
+  methods: {
+    show_message() {
+      this.message = this.message === 'hello world' ? '你好世界' : 'hello world';
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
